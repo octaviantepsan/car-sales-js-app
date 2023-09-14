40f96e70464c8fbf3a7c0d4da43847d3c4586f3a
Octavian Tepsan <tepsanoctavian@gmail.com>
2023-09-14 19:44:00 +0300

    Added pictures and prepared for introducing js

--- main/scripts/main.js
+++ main/scripts/main.js
@@ -1,1 +1,28 @@
 const cars = ["Dacia", "BMW", "Mitsubishi", "Renault", "Lamborghini", "Ford"];
+
+const anunturi = [
+    {
+        marca_masina: "Dacia",
+        url_imagine: "../main/images/Dacia.jpg"
+    },
+    {
+        marca_masina: "BMW",
+        url_imagine: "../main/images/BMW.jpg"
+    },
+    {
+        marca_masina: "Mitsubishi",
+        url_imagine: "../main/images/Mitsubishi.jpg"
+    },
+    {
+        marca_masina: "Renault",
+        url_imagine: "../main/images/Renault.jpg"
+    },
+    {
+        marca_masina: "Lamborghini",
+        url_imagine: "../main/images/Lamborghini.jpg"
+    },
+    {
+        marca_masina: "Ford",
+        url_imagine: "../main/images/Ford.jpg"
+    }
+]
