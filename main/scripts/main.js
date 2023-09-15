9be3bb53f571dab7bf5499d289f839f0fecab67e
Octavian Tepsan <tepsanoctavian@gmail.com>
2023-09-15 17:39:59 +0300

    Automated the filter and the articles

--- main/scripts/main.js
+++ main/scripts/main.js
@@ -1,5 +1,3 @@
-const cars = ["Dacia", "BMW", "Mitsubishi", "Renault", "Lamborghini", "Ford"];
-
 const anunturi = [
     {
         marca_masina: "Dacia",
@@ -11,7 +9,7 @@ const anunturi = [
     },
     {
         marca_masina: "Mitsubishi",
-        url_imagine: "../main/images/Mitsubishi.jpg"
+        url_imagine: "../main/images/Mitsubishi1.jpg"
     },
     {
         marca_masina: "Renault",
@@ -24,5 +22,57 @@ const anunturi = [
     {
         marca_masina: "Ford",
         url_imagine: "../main/images/Ford.jpg"
+    },
+    {
+        marca_masina: "Honda",
+        url_imagine: "../main/images/Honda.jpg"
     }
 ]
+
+const marci = [
+    {
+        marca_masina: "Dacia",
+    },
+    {
+        marca_masina: "BMW",
+    },
+    {
+        marca_masina: "Mitsubishi",
+    },
+    {
+        marca_masina: "Renault",
+    },
+    {
+        marca_masina: "Lamborghini",
+    },
+    {
+        marca_masina: "Ford",
+    }
+]
+
+// < div class="articles" id = "articles" ></div >
+
+
+// <div class="anunt" id="anunt">
+//     <img class="carImage" src="../main/images/Dacia.jpg">
+//         <p class="carBrand"></p>
+// </div>
+
+
+function populateFilter() {
+    if (document.getElementById("filter") !== null) {
+        for (let i = 0; i < marci.length; i++) {
+            document.getElementById("filter").innerHTML += `<option>${marci[i].marca_masina}</option>`;
+        }
+    }
+}
+populateFilter();
+
+function populateArticles() {
+    if (document.getElementById("articles") !== null) {
+        for (let i = 0; i < anunturi.length; i++) {
+            document.getElementById("articles").innerHTML += `<div class="anunt"><img class="carImage" src="${anunturi[i].url_imagine}"><p class="carBrand">${anunturi[i].marca_masina}</p></div>`;
+        }
+    }
+}
+populateArticles();
