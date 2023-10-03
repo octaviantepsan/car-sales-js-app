5375cebaa8500eeefd6fe9a9c55035149573f1f5
Octavian Tepsan <tepsanoctavian@gmail.com>
2023-10-03 20:19:30 +0300

    Added Event Listener to the filter

--- main/scripts/main.js
+++ main/scripts/main.js
@@ -27,7 +27,7 @@ const anunturi = [
         marca_masina: "Honda",
         url_imagine: "../main/images/Honda.jpg"
     }
-]
+];
 
 const marci = [
     {
@@ -48,22 +48,44 @@ const marci = [
     {
         marca_masina: "Ford",
     }
-]
+];
 
 function populateFilter() {
     if (document.getElementById("filter") !== null) {
+        document.getElementById("filter").innerHTML += "<option disabled selected></option>";
         for (let i = 0; i < marci.length; i++) {
             document.getElementById("filter").innerHTML += `<option>${marci[i].marca_masina}</option>`;
         }
     }
 }
-populateFilter();
 
 function populateArticles() {
     if (document.getElementById("articles") !== null) {
         for (let i = 0; i < anunturi.length; i++) {
-            document.getElementById("articles").innerHTML += `<div class="anunt"><img class="carImage" src="${anunturi[i].url_imagine}"><p class="carBrand">${anunturi[i].marca_masina}</p></div>`;
+            document.getElementById("articles").innerHTML +=
+                `<div class="anunt"><img class="carImage" src="${anunturi[i].url_imagine}"><p class="carBrand">${anunturi[i].marca_masina}</p></div>`;
         }
     }
 }
+
+function createEventListeners() {
+    let $filter = document.getElementById("filter");
+    if ($filter !== null) {
+        $filter.addEventListener("change", function (event) {
+            if (event.target.value !== null) {
+                for (let i = 0; i < anunturi.length; i++) {
+                    if (anunturi[i].marca_masina === event.target.value) {
+                        document.getElementById("articles").innerHTML = 
+                        `<div class="anunt"><img class="carImage" src="${anunturi[i].url_imagine}"><p class="carBrand">${anunturi[i].marca_masina}</p></div>`;
+                    }
+                }
+            }
+
+        });
+    }
+}
+
+
+populateFilter();
 populateArticles();
+createEventListeners();
