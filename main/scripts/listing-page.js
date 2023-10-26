e037771bd06253adff3e73b3915456e16e1f2dfd
Octavian Tepsan <tepsanoctavian@gmail.com>
2023-10-26 21:23:58 +0300

    Slideshow images integration with js

--- main/scripts/listing-page.js
+++ main/scripts/listing-page.js
@@ -1,14 +1,39 @@
+let listingDataAsString = window.sessionStorage.getItem("listingData");
+let listingData = JSON.parse(listingDataAsString);
 let slideIndex = 1;
 
-function getData() {
-    let listingDataAsString = window.sessionStorage.getItem("listingData");
-    let listingData = JSON.parse(listingDataAsString);
-    console.log(listingData);
+function showInformation() {
+    let imageCounter = 1;
+    let i;
+
+    for (i = 0; i < listingData.anunt_imagini.length; i++) {
+        document.getElementById("slideshow-container").innerHTML += `<div class="mySlides fade"><div class="numbertext">${imageCounter} / ${listingData.anunt_imagini.length}</div><img class="imageOfSlide" src="${listingData.anunt_imagini[i]}"></div>`;
+        imageCounter++;
+    }
+
+    imageCounter = 1;
+    document.getElementById("slideshow-container").innerHTML += "<div class='imageDots' id='imageDots'></div>"
+
+    for (i = 0; i < listingData.anunt_imagini.length; i++) {
+        document.getElementById("imageDots").innerHTML += `<span class="dot" onclick="currentSlide(${imageCounter})"></span>`;
+        imageCounter++;
+    }
+
+    document.getElementById("anuntTitluText").innerHTML = listingData.anunt_titlu;
+    document.getElementById("anuntDescriereText").innerHTML = listingData.anunt_descriere;
+
+    document.getElementById("Brand").innerHTML += `<span class="listingInfo">${listingData.marca_masina}</span>`;
+    document.getElementById("Displacement").innerHTML += `<span class="listingInfo">${listingData.engine_displacement}</span>`;
+    document.getElementById("Year").innerHTML += `<span class="listingInfo">${listingData.year}</span>`;
+    document.getElementById("Power").innerHTML += `<span class="listingInfo">${listingData.power}</span>`;
+    document.getElementById("Mileage").innerHTML += `<span class="listingInfo">${listingData.km}</span>`;
+    document.getElementById("Phone number").innerHTML += `<span class="listingInfo">${listingData.phone_number}</span>`;
 }
 
 function showSlides(n) {
     let i;
     let $slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
 
     if (n > $slides.length) {
         slideIndex = 1;
@@ -20,8 +45,11 @@ function showSlides(n) {
     for (i = 0; i < $slides.length; i++) {
         $slides[i].style.display = "none";
     }
-
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     $slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 // Next/previous controls
@@ -34,36 +62,14 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showInformation() {
-    let listingDataAsString = window.sessionStorage.getItem("listingData");
-    let listingData = JSON.parse(listingDataAsString);
-    let imageIdentifier = 1;
-
-    for(let i = 0; i < listingData.anunt_imagini.length; i++) {
-        document.getElementById(`imageOfSlide${imageIdentifier}`).src = listingData.anunt_imagini[i];
-        imageIdentifier++;
-    }
-    
-    document.getElementById("anuntTitluText").innerHTML = listingData.anunt_titlu;
-    document.getElementById("anuntDescriereText").innerHTML = listingData.anunt_descriere;
-
-    document.getElementById("Brand").innerHTML += `<span class="listingInfo">${listingData.marca_masina}</span>`;                               
-    document.getElementById("Displacement").innerHTML += `<span class="listingInfo">${listingData.engine_displacement}</span>`;
-    document.getElementById("Year").innerHTML += `<span class="listingInfo">${listingData.year}</span>`;
-    document.getElementById("Power").innerHTML += `<span class="listingInfo">${listingData.power}</span>`;
-    document.getElementById("Mileage").innerHTML += `<span class="listingInfo">${listingData.km}</span>`;
-    document.getElementById("Phone number").innerHTML += `<span class="listingInfo">${listingData.phone_number}</span>`;
-}
-
 function createEventListeners() {
     let $return = document.getElementById("returnButton");
 
-    $return.addEventListener("click", function(){
+    $return.addEventListener("click", function () {
         window.location.href = redirectUrls.index;
     })
 }
 
 createEventListeners();
-showSlides(slideIndex);
 showInformation();
-getData();
+showSlides(slideIndex);
